0afe5b2a531fd161195f4e1399a70011878dda18
Anderson Oliveira <andervilo@gmai.com>
2024-09-18 11:56:16 -0300

    fix: ajustes de tipos de campo da tabela contas

--- src/main/resources/db/migration/V1__create_table_contas.sql
+++ src/main/resources/db/migration/V1__create_table_contas.sql
@@ -1,7 +1,7 @@
-CREATE TABLE contas_a_pagar (
-    id SERIAL PRIMARY KEY,
+CREATE TABLE contas (
+    id BIGSERIAL PRIMARY KEY,
     data_vencimento DATE NOT NULL,
-    data_pagamento DATE,
+    data_pagamento TIMESTAMP,
     valor DECIMAL(10, 2) NOT NULL,
     descricao TEXT,
     situacao VARCHAR(50) NOT NULL
