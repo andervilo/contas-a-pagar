a52dc0373a6509de134afc9d32a00fb7b94a1e98
Anderson Oliveira <andervilo@gmai.com>
2024-09-18 11:35:43 -0300

    feature: criando controller de autenticação

---
+++ src/main/java/com/anderson/contasapagar/api/auth/AuthController.java
@@ -0,0 +1,47 @@
+package com.anderson.contasapagar.api.auth;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.anderson.contasapagar.api.dto.AuthLogin;
+
+@RequestMapping("/auth/token")
+@RestController
+public class AuthController {
+
+    @Value("${config.keycloak.loginurl}")
+    private String keycloakLoginUrl;
+
+    @PostMapping
+    public ResponseEntity<String> token(@RequestBody AuthLogin user) {
+
+
+        HttpHeaders headers = new HttpHeaders();
+        RestTemplate rt = new RestTemplate();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+        formData.add("client_id", user.clientId());
+        formData.add("username", user.username());
+        formData.add("password", user.password());
+        formData.add("grant_type", user.grantType());
+
+        HttpEntity<MultiValueMap<String, String>> entity
+         = new HttpEntity<MultiValueMap<String,String>>(formData, headers);
+        
+       var result = rt.postForEntity(keycloakLoginUrl, entity, String.class);
+    
+        return result;
+    }
+    
+}
